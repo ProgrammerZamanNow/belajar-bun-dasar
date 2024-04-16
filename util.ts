fbe432287ca4009b163cd9aaae3f073bf4bbfdb4
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-04-16 10:16:24 +0700

    Add Bun Configuration details logging
    Added a new util.ts file to log Bun library's version, revision, and current database configuration details. This will provide better visibility and easier debugging.

---
+++ util.ts
@@ -0,0 +1,7 @@
+console.info(Bun.version);
+console.info(Bun.revision);
+console.info(Bun.env.DATABASE_HOST);
+console.info(Bun.env.DATABASE_PORT);
+console.info(Bun.env.DATABASE_USER);
+console.info(Bun.env.DATABASE_PASSWORD);
+console.info(Bun.env.DATABASE_NAME);
